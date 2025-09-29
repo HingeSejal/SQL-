268c5307d137e8baac05fbaeddd0e0061db98ce0
HingeSejal <sejalhinge4@gmail.com>
2025-09-29 17:53:17 +0530

    learnt

--- BARAA/DDL/advanceJoin.sql
+++ BARAA/DDL/advanceJoin.sql
@@ -13,3 +13,19 @@ from customers as c
 left join orders as o
 on c.id = o.customer_id 
 where o.customer_id  is NULL;
+
+-- b] Right Anti Join - return rows from right that has no match in left
+
+-- ex.customer that havent placed any order
+
+select *
+from customers as c
+left join orders as o
+on c.id = o.customer_id 
+where c.id is NOT NULL;
+
+-- c} cross join
+-- everything in A is connected to everything in B 
+select *
+from customers 
+cross join orders;
