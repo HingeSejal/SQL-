2ff6f6081e575d436b369494a18fd836bf27df5c
HingeSejal <sejalhinge4@gmail.com>
2026-03-10 13:05:39 +0530

    cursors in sql

--- PLSQL/mysql/cursor.sql
+++ PLSQL/mysql/cursor.sql
@@ -1,31 +1,81 @@
  -- it is database obj
  -- temporary wokrarea in memory
  -- it is created when sql is executed inside procedure or functions
+ -- Resultset rs = "select * from candidate"
+ -- Cursor rs = "select * from candidate"
 
- DELIMITER $$
- create procedure add_student(in prno int, in prname varchar(30), in pper float)
- tca:BEGIN
-        DECLARE tgrade varchar(30);
-        DECLARE cnt int;    
-        
-        SELECT COUNT(*) INTO cnt FROM Candidate WHERE rno = prno;
-        
-        IF cnt > 0 THEN
-            SELECT CONCAT('Candidate', prno, ' Record Already Exists') AS message;
-            LEAVE TCA; -- Exit the procedure
-        END IF;
-        
-        IF pper > 90 THEN
-            SET tgrade = 'Distinction';
-        ELSEIF pper >= 60 THEN      
-            SET tgrade = 'First Class';
-        ELSEIF pper >= 40 THEN
-            SET tgrade = 'Pass';
-        ELSE
-            SET tgrade = 'Fail';
-        END IF;
-        
-        INSERT INTO Candidate (rno, name, per, grade) VALUES (prno, prname, pper, tgrade);
-        
-        SELECT CONCAT('Candidate ', prname, ' Record Inserted Successfully') AS message;
-    END $$
+ --result stored in by cursor is called active_set
+ --CURSOR is used to fetch data one by one from active_set
+
+ --Select Query That we write to fetch the data is cursor query
+
+ --Two types of cursor 
+    --1. implicit cursor - automatically created by mysql when we execute any sql query inside procedure or function
+    --2. explicit cursor - we have to create explicitly by using cursor statement
+
+ 
+DELIMITER $$
+
+CREATE procedure show_student()
+
+BEGIN
+    declare tid int ;
+    declare tname varchar(20);
+    declare tmarks int; 
+    
+    declare is_done int default false;
+
+    DECLARE stud_cursor CURSOR for 
+        select id , name , marks from stud;
+
+    declare continue handler for not found 
+        set is_done =true;
+
+    open stud_cursor;
+    TCA:LOOP
+
+        fetch stud_cursor into tid , tname , tmarks;
+
+        if is_done then
+            leave TCA;
+        end if;
+
+        select concat(' ID :',tid , ' Name:',tname , ' Marks:',tmarks) as msg;
+
+    END LOOP;
+    CLOSE stud_cursor;
+End $$
+
+delimiter ;
+
+CALL show_student();
+
++------------------------+
+| msg                    |
++------------------------+
+| ID :1 Name:John Marks:85 |
++------------------------+
+1 row in set (0.00 sec)
+
++-------------------------+
+| msg                     |
++-------------------------+
+| ID :2 Name:Alice Marks:90 |
++-------------------------+
+1 row in set (0.01 sec)
+
++------+
+| msg  |
++------+
+| NULL |
++------+
+1 row in set (0.01 sec)
+
++------------------------+
+| msg                    |
++------------------------+
+| ID :4 Name:Emma Marks:88 |
++------------------------+
+1 row in set (0.01 sec)
+
+Query OK, 0 rows affected (0.01 sec)
--- PLSQL/postgresql/cursor.sql
+++ PLSQL/postgresql/cursor.sql
@@ -2,34 +2,27 @@
  -- temporary wokrarea in memory
  -- it is created when sql is executed inside procedure or functions
  
-CREATE or replace procedure add_student(prno int, prname varchar, pper float)
-LANGUAGE plpgsql    
-AS $$
+create or replace PROCEDURE show_student()
+language plpgsql 
+as $$
 DECLARE
-    tgrade varchar;
-    cnt int;
+    stud_cursor CURSOR FOR SELECT rno , name , grade FROM stud;
+    stud_rec record;
 BEGIN
-    SELECT COUNT(*) INTO cnt FROM Candidate WHERE rno = prno;
-    IF cnt > 0 THEN
-        RAISE NOTICE 'Candidate % Record Already Exists', prno;
-        RETURN; -- Exit the procedure
-    END IF;
+    OPEN stud_cursor;
+    LOOP
+    fetch stud_cursor into stud_rec;
+    exit when not found;
+    RAISE NOTICE 'Rno: %, Name: %, Grade: %', stud_rec.rno, stud_rec.name, stud_rec.grade;
+    END LOOP;
 
-    IF pper > 90 THEN
-        tgrade := 'Distinction';
-    ELSIF pper >= 60 THEN      
-        tgrade := 'First Class';
-    ELSIF pper >= 40 THEN
-        tgrade := 'Pass';
-    ELSE
-        tgrade := 'Fail';
-    END IF;
+    close stud_cursor;
+end;
+$$ ;
 
-    INSERT INTO Candidate (rno, name, per, grade) VALUES (prno, prname, pper, tgrade);
-
-    RAISE NOTICE 'Candidate % Record Inserted Successfully', prname;
-END
-$$;
-
-DELIMITER ;
-
+call show_student();
+NOTICE:  Rno: 1, Name: Rohit, Grade: Distinction
+NOTICE:  Rno: 2, Name: Rahul, Grade: First Class
+NOTICE:  Rno: 3, Name: Ramesh, Grade: Pass
+NOTICE:  Rno: 4, Name: Ramesh, Grade: Pass
+CALL
